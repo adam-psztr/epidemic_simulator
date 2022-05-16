f21edfb678ff34cacd8ac99c8dfc5553e7faa351
Pasztor Adam <adam.psztr@icloud.com>
2022-05-16 14:40:35 +0200

    programming infection logic

--- epidemic_sim.py
+++ epidemic_sim.py
@@ -10,6 +10,11 @@ PERSON_COLOR_CLEAR = (200,150,0)
 PERSON_COLOR_INFECTED = (200,0,0)
 PERSON_COLOR_IMMUNE =(0,150,0)
 PERSON_AURA = 20
+ILLNESS_TIME = 500
+IMMUNE_TIME = 800
+INFECTION_PROBABILITY_WITH_MASK = 50
+INFECTION_PROBABILITY_WITHOUT_MASK = 2
+FIRST_CONTACT = 2
 
 # Initialization
 pygame.init()
@@ -25,6 +30,9 @@ class Person:
 		self.y=y 
 		self.dx = randint(-1,1)
 		self.dy = randint(-1,1)
+		self.illness = 0
+		self.immune = 0
+		self.mask = False
 
 	def move(self):
 		if randint(1,30) == 1:
@@ -36,20 +44,50 @@ class Person:
 		if PERSON_AURA < self.y + self.dy < HEIGHT-PERSON_AURA:
 			self.y += self.dy
 
+	def cure(self):
+		if self.illness > 0:
+			self.illness -= 1
+			if self.illness == 0:
+				self.immune = IMMUNE_TIME
+		if self.immune > 0:
+			self.immune -= 1
+
 def modify(people):
 	for person in people:
 		person.move()
+		person.cure()
+		
+		for other_person in people:
+			if id(person) != id(other_person):
+				if other_person.x - PERSON_AURA*2 < person.x < other_person.x+2*PERSON_AURA \
+					and other_person.y - PERSON_AURA*2 < person.y < other_person.y+2*PERSON_AURA:
+					if other_person.illness > 0 and person.illness == 0 and person.immune == 0:
+						if person.mask == True or other_person == True:
+							if randint(1, INFECTION_PROBABILITY_WITH_MASK) == 1:
+								person.illness = ILLNESS_TIME
+						else:
+							if randint(1, INFECTION_PROBABILITY_WITHOUT_MASK) == 1:
+								person.illness = ILLNESS_TIME
 
 def draw(people):
 	screen.fill((0,0,0))
 	for person in people:
-		pygame.draw.circle(screen, PERSON_COLOR_CLEAR, (person.x, person.y), PERSON_AURA, 0)
+		if person.illness > 0:
+			_color = PERSON_COLOR_INFECTED
+		elif person.immune > 0:
+			_color = PERSON_COLOR_IMMUNE
+		else:
+			_color = PERSON_COLOR_CLEAR
+		pygame.draw.circle(screen, _color, (person.x, person.y), PERSON_AURA, 0)
 	pygame.display.update()
 	fps.tick(PERSON_SPEED)
 
-
 # Create people
 main_people_list = [Person(randint(50, WIDTH-50), randint(50, HEIGHT-50)) for i in range(NUMBER_OF_PEOPLE)]
+
+# The first infection
+for i in range(FIRST_CONTACT):
+	main_people_list[i].illness = ILLNESS_TIME
 
 # Main program
 while True:
